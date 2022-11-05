03a2b74f895f5cad9abcada2fd746a173b994731
Jeremy Breidenbach <jeremy.breidenbach@gmail.com>
2022-11-04 23:08:46 -0500

    feat: add player and falling movement

--- src/maps/Background.gd
+++ src/maps/Background.gd
@@ -1,24 +1,36 @@
 # class_name
 
-extends %BASE%
-
+extends Sprite
 
 # signals
+
 # enums
+
 # constants
+const VELOCITY = 15
+
 # exported variables
+
 # public variables
+var texture_height = 0
+
 # private variables
+
 # onready variables
 
 
 # built in _ready method
 # Called when the node enters the scene tree for the first time.
-func _ready()%VOID_RETURN%:
-	pass # Replace with function body.
+func _ready():
+	texture_height = texture.get_size().y * scale.y
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta%FLOAT_TYPE%)%VOID_RETURN%:
-	#pass
+func _process(delta):
+	position.y -= VELOCITY
+	reposition_sprite()
 
+
+func reposition_sprite():
+	if position.y < -texture_height:
+		position.y += 2 * texture_height
---
+++ src/player/Player.gd
@@ -0,0 +1,38 @@
+# class_name
+
+extends KinematicBody2D
+
+# signals
+
+# enums
+
+# constants
+
+# exported variables
+export(int) var MOVE_SPEED = 800
+
+# public variables
+var velocity = Vector2.ZERO
+
+# private variables
+
+# onready variables
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+func _physics_process(delta):
+	get_input()
+	velocity = move_and_slide(velocity)
+
+
+func get_input():
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+
+	velocity = velocity.normalized() * MOVE_SPEED
